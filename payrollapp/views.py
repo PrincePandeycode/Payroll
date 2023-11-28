41ae14bee37f48a91ac3e9344e528ab3efb297a8
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-29 00:00:50 +0530

    the links on home are connected to both login page and signup page using the name parameter in urls

--- payrollapp/views.py
+++ payrollapp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def signup(request):
+    return render(request,"signup.html")
